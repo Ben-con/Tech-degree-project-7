f6868f900aae9fb7d6e25e0fd9ca1c657d3e1382
Ben.con <041ymoob@gmail.com>
2021-05-19 12:03:29 -0700

    adding charts

---
+++ app.js
@@ -0,0 +1,57 @@
+const alert = document.getElementById('banner')
+const trafficCanvas = document.getElementById('traffic-chart')
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+ });
+
+let trafficData = {
+    labels:["16-22","23-29","30-5","6-12","13-19","20-26","27-3","4-10","11-17","18-24","25-31"],
+    dataSets:[{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+            2500],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+    }]
+};
+
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+    duration: 0
+    },
+    scales: {
+    y: {
+    beginAtZero: true
+    }
+    },
+    plugins: {
+        legend: {
+            display: false
+            }
+            }
+            };
+
+
+ 
+
+// ------------------------- Making alert banner ----------------------
+alert.innerHTML =
+`
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`
+
+alert.addEventListener('click', e =>{
+    const element = e.target;
+    if(element.classList.contains = ('alert-banner-close')){
+        banner.style.display = "none"
+    }
+})
+// --------------------------------------------------------------------
